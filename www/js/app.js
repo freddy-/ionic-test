a88e09107b724697d2da73574347636152154ee8
Frederico Souza <fresouza0@gmail.com>
2017-05-07 19:51:03 -0300

    Adicionada biblioteca para previnir que a tela apague enquanto o app estiver aberto

--- www/js/app.js
+++ www/js/app.js
@@ -8,28 +8,26 @@ angular.module('app', ['ionic', 'ngCordova'])
 .controller('ListBTController', function($scope, $ionicPlatform, $ionicLoading, $location){
   $ionicPlatform.ready(function() {
 
+    //TODO verificar se isto esta 100% certo
+    window.plugins.insomnia.keepAwake();
+
     bluetoothSerial.isEnabled(function () {
-      console.log("Bluetooth is Enabled.");
+
     }, function (reason) {
-    console.log("Bluetooth is *not* Enabled.");
       //abre a config de bluetooth
       bluetoothSerial.showBluetoothSettings(function(){},function(){});
     });
 
     bluetoothSerial.isConnected(
-      function() {
-          console.log("Bluetooth is connected");
-          
+      function() {          
           //mudar pra tela do gauge
           $location.path('/gauge');
       },
 
       function() { //nao esta conectado
-        console.log("nao conectado");
 
         $scope.$apply(function () {
           $scope.select = function(btDevice){
-            console.log(btDevice);
 
             $ionicLoading.show({template: 'Conectando...'});
 
@@ -41,18 +39,6 @@ angular.module('app', ['ionic', 'ngCordova'])
                   //mudar pra tela do gauge
                   $location.path('/gauge');
                 });
-                
-                /*
-                //aplica um listener para quando o dispositivo enviar dados
-                bluetoothSerial.subscribe('-', 
-                  function(data){
-                    console.log(data);
-                  },
-                  function(){
-
-                  }
-                );
-                */
 
               }, function(){
                 console.log("fail!");
@@ -84,11 +70,10 @@ angular.module('app', ['ionic', 'ngCordova'])
   }
 
   $ionicPlatform.ready(function() {
-    console.log("ready");  
-
     $scope.calculateRotations = function(rpm){
 
-      console.log(rpm);
+      //segundo a documentação do sensor de rotação e PMS, a frequencia lida do sensor deve ser multiplicada por 30
+      rpm = rpm * 5; //constante do sensor de rotação e PMS - 300 RPM = 10Hz
 
       //TODO calcular RPM para graus
       //0 a 9000 RPM
@@ -104,8 +89,6 @@ angular.module('app', ['ionic', 'ngCordova'])
         anguloEscala = map(rpm, 1000.0, 9000.0, 325.0, 440.0);
         anguloAgulha = map(rpm, 1000.0, 9000.0, 297.0, 437.0);
       }
-
-      console.log(JSON.stringify({escala: anguloEscala, agulha: anguloAgulha}));
 
       $scope.escala = anguloEscala;
       $scope.agulha = anguloAgulha;
@@ -123,33 +106,6 @@ angular.module('app', ['ionic', 'ngCordova'])
 
       });
     }
-
-    //neste ponto o android já estará conectado ao dispositivo bluetooth
-    //resta receber os dados
-/*
-    var dadosRecebidos = new Array();
-    bluetoothSerial.subscribeRawData( 
-      function(data){
-        var bytes = new Uint8Array(data);
-        console.log(JSON.stringify(bytes));
-        /*
-        for(var val; bytes.length; val++){
-          if(bytes[val] == 45){
-            console.log(JSON.stringify(dadosRecebidos));
-            dadosRecebidos = new Array();
-            console.log("FIM")
-          }else{
-            dadosRecebidos.push(bytes[val]);
-          }
-        }
-        
-
-      },
-      function(){
-
-      }
-    );
-*/
   });
 
   $scope.rangeval = 0;
@@ -178,56 +134,9 @@ angular.module('app', ['ionic', 'ngCordova'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
 	
-	//http://stackoverflow.com/questions/22661494/view-ionic-mobile-app-on-fullscreen
-  //esconde a statusbar
-	StatusBar.hide();
-/*
-    //https://github.com/don/BluetoothSerial/issues/181
-    //verifica se o bluetooth esta habilitado
-    bluetoothSerial.isEnabled(function () {
-      console.log("Bluetooth is Enabled.");
-    }, function (reason) {
-      console.log("Bluetooth is *not* Enabled.");
-      //abre a config de bluetooth
-      bluetoothSerial.showBluetoothSettings(function(){},function(){});
-    });
-
-
-    //lista os dispositivos pareados
-    bluetoothSerial.list(
-      function(list){
-        console.log("\n\n");
-        console.log("dispositivos encontrados");
-
-        for (var i = list.length - 1; i >= 0; i--) {
-          console.log(list[i].name + " " + list[i].address);
-        };
-
-        //conecta com o dispositivo com MAC ADDR
-        bluetoothSerial.connect("20:14:08:26:25:64",
-          function(){
-            console.log("conectado");
-
-            //aplica um listener para quando o dispositivo enviar dados
-            bluetoothSerial.subscribe('-', function(data){
-                console.log(data);
-            }, function(){
-
-            });
-
-            //envia dados para o dispositivo
-            bluetoothSerial.write("hello-", function(){}, function(){});
-
-          },function(){
-            console.log("fail!");
-          });
-
-      }, function(){
-          console.log("fail!");
-      });
-    
-*/
-
+    //http://stackoverflow.com/questions/22661494/view-ionic-mobile-app-on-fullscreen
+    //esconde a statusbar
+    StatusBar.hide();
   });
 
 });
